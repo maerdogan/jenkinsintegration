3318036327e083be36603c3e8173f80180e2ffb1
maerdogan <56476441+maerdogan@users.noreply.github.com>
2022-06-20 00:16:36 +0300

    Update test-mysql.py

--- cmds-sqls/test-mysql.py
+++ cmds-sqls/test-mysql.py
@@ -1,22 +1,11 @@
 import mysql.connector
-from mysql.connector import errorcode
 
-# Obtain connection string information from the portal
+mydb = mysql.connector.connect(
+  host='10.130.4.218',
+  user="root",
+  password="admin"
+)
 
-connection = mysql.connect(host='10.130.4.218',
-                             user='root',
-                             password='admin',
-                             db='mysqldb',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+print(mydb)
 
-# Construct connection string
-try:
-    with connection.cursor() as cursor:
-        # Read a single record
-        sql = "SELECT `id`, `firstname`,`lastname` FROM `users` WHERE `id`=%s"
-        cursor.execute(sql, ('2',))
-        result = cursor.fetchone()
-        print(result)
-finally:
-    connection.close()
+
